75538af7ee9aaefb514d6bf88b45adc39e01ccef
Nandan Rao <nandanmarkrao@gmail.com>
2017-05-14 13:12:47 +0200

    Sparse ElasticNet implementation.

--- personality.py
+++ personality.py
@@ -7,6 +7,7 @@ from os import path
 from sklearn.decomposition import SparsePCA
 from sklearn.preprocessing import scale
 from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.linear_model import Lasso, ElasticNet
 
 def get_question_num(s, q):
     try:
@@ -43,9 +44,69 @@ def prep_X(df):
 
     return df
 
+def get_big_five_comps(bigfive):
+    codes = bigfive.bigfive_code
+    return np.matrix([(codes == i).astype(int) for i in codes.unique()]).T
+
 def big_five_projection(df):
     bigfive = pd.read_csv("educatalyst/Auxil/q1_key_bigfive.csv")
-    codes = bigfive.bigfive_code
     df = df.filter(regex='^1')
-    comps = np.matrix([(codes == i).astype(int) for i in codes.unique()])
-    return df.as_matrix().dot(comps.T)
+    comps = get_big_five_comps(bigfive)
+    return df.as_matrix().dot(comps)
+
+class MultiLasso(Lasso):
+    def __init__(self, alpha=1.0, **kwargs):
+        # self.kwargs = kwargs
+        # self.kwargs['alpha'] = alpha
+        self.alpha = alpha
+
+    def fit(self, X, Y):
+        cols = Y.shape[1]
+        r = range(cols)
+        # print self.kwargs
+        self.models = [Lasso(alpha = self.alpha).fit(X, Y[:,i]) for i in r]
+        self.coef_ = [m.coef_ for m in self.models]
+        self.original_var = [(np.asarray(Y[:,i] - Y[:,i].mean()) ** 2).sum() for i in r]
+
+
+    def predict(self, X):
+        if not self.models:
+            raise Exception('bogus! No models???')
+        return np.matrix([m.predict(X) for m in self.models]).T
+
+    def scores(self, X, Y):
+        if not self.models:
+            raise Exception('bogus! No models???')
+        r = range(Y.shape[1])
+        return [self.models[i].score(X, Y[:,i]) for i in r]
+
+    def score(self, X, Y):
+        return np.mean(self.scores(X, Y))
+
+class MultiElasticNet(ElasticNet):
+    def __init__(self, alpha=1.0, l1_ratio = 0.5, **kwargs):
+        self.alpha = alpha
+        self.l1_ratio = l1_ratio
+
+    def fit(self, X, Y):
+        cols = Y.shape[1]
+        r = range(cols)
+        self.models = [ElasticNet(alpha = self.alpha, l1_ratio = self.l1_ratio)
+                       .fit(X, Y[:,i]) for i in r]
+        self.coef_ = [m.coef_ for m in self.models]
+        self.original_var = [(np.asarray(Y[:,i] - Y[:,i].mean()) ** 2).sum() for i in r]
+        return self
+
+    def predict(self, X):
+        if not self.models:
+            raise Exception('bogus! No models???')
+        return np.matrix([m.predict(X) for m in self.models]).T
+
+    def scores(self, X, Y):
+        if not self.models:
+            raise Exception('bogus! No models???')
+        r = range(Y.shape[1])
+        return [self.models[i].score(X, Y[:,i]) for i in r]
+
+    def score(self, X, Y):
+        return np.mean(self.scores(X, Y))
