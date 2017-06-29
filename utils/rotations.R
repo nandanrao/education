1ccf703e49413ee6072daba9714c963633717d5a
Nandan Rao <nandanmarkrao@gmail.com>
2017-06-29 08:30:16 +0200

    Folder organization

---
+++ utils/rotations.R
@@ -0,0 +1,3 @@
+bigfive <- read.csv("clean_data/bigfive.csv")
+bigfive.survey <- read.csv("clean_data/bigfive_survey.csv")
+others <- read.csv("clean_data/others.csv")
---
+++ utils/test_personality.py
@@ -0,0 +1,8 @@
+# import pandas as pd
+# from personality import *
+
+# def test_prep_X():
+#     # create with 300 and 400's,
+#     # ensure the variance is somehow what you want... ?
+#     # Like to the bigfive?? Scale it to 1-5???
+#     return False
