f347035274673501f71b4816f6b503e0f0844814
Nandan Rao <nandanmarkrao@gmail.com>
2018-01-15 12:49:59 +0100

    reverting initial-erp to previous run

--- thesis/auto/thesis.el
+++ thesis/auto/thesis.el
@@ -5,6 +5,19 @@
                      '(("article" "a4paper" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("biblatex" "backend=biber" "citestyle=authoryear") ("algpseudocode" "noend")))
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -29,6 +42,7 @@
     "algpseudocode")
    (TeX-add-symbols
     '("diagmat" 1)
+    "argmin"
     "X"
     "F"
     "Load"
@@ -40,12 +54,10 @@
     "fig:grid-others"
     "fig:pls-svd"
     "fig:regression")
-   (LaTeX-add-environments
-    '("proof" LaTeX-env-args ["argument"] 0)
-    '("prop" LaTeX-env-args ["argument"] 0)
+   (LaTeX-add-bibliographies)
+   (LaTeX-add-amsthm-newtheorems
     "theorem"
     "proposition"
-    "lemma")
-   (LaTeX-add-bibliographies))
+    "lemma"))
  :latex)
 
